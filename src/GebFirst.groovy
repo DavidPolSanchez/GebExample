fc132f97b988f570bbf5d799d6a90b1291fb62b6
DavidPolSanchez <davidpolsanchezm98@gmail.com>
2023-03-07 15:23:19 +0100

    add other browsers

--- src/GebFirst.groovy
+++ src/GebFirst.groovy
@@ -1,9 +1,15 @@
 import geb.Browser
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.ie.InternetExplorerDriver
+import org.openqa.selenium.edge.EdgeDriver
+import org.openqa.selenium.safari.SafariDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.*
 
 def browser = new Browser(driver : new ChromeDriver())
             //new Browser(driver : new FirefoxDriver())
+            //new Browser(driver : new SafariDriver
+            //new Browser(driver : new EdgeDriver())
+            //new Browser(driver : new InternetExplorerDriver())
 
 browser.go("http://udemy.com")

246364f8a9ddb65556d71fac3dc3ba990192d155
DavidPolSanchez <davidpolsanchezm98@gmail.com>
2023-03-07 16:33:47 +0100

    add readme

--- src/test/GebFirst.groovy
+++ src/test/GebFirst.groovy
@@ -1,3 +1,5 @@
+package test
+
 import geb.Browser
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.ie.InternetExplorerDriver
@@ -8,8 +10,8 @@ import org.openqa.selenium.*
 
 def browser = new Browser(driver : new ChromeDriver())
             //new Browser(driver : new FirefoxDriver())
-            //new Browser(driver : new SafariDriver
-            //new Browser(driver : new EdgeDriver())
+            //new Browser(driver : new SafariDriver())
+            //new Browser(driver : new EdgeDriver()))
             //new Browser(driver : new InternetExplorerDriver())
 
 browser.go("http://udemy.com")
